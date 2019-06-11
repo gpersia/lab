f83ae87693fefba87376e04b7b671b07c5145c56
gpersia <gpersia1998@gmail.com>
2019-06-11 10:30:52 -0300

    tp2

--- alumnos/56009-Gianluca-Persia/tp2/tp2.py
+++ alumnos/56009-Gianluca-Persia/tp2/tp2.py
@@ -9,7 +9,7 @@ listab = []
 listah = []
 cond = threading.Condition()
 
-def hospital():
+def seguridad():
         while listab.count("R") == 3 or listab.count("B") == 3:
                 listah.append(listab.pop(0))
                 listab.append(listah.pop(0))
@@ -22,14 +22,14 @@ def a_bordo():
                 print("Sube hincha RIVER")
         else:
                 print("Sube hincha BOCA")
-        time.sleep(1)
+        #time.sleep(1)
         listab.append(subeh)
         if len(listab) == 4:
-                hospital()
+                seguridad()
                 print("Lleno")
-                time.sleep(1)
+                #time.sleep(1)
                 a_remar()
-                time.sleep(1)
+                #time.sleep(1)
         cond.release()
 
 def a_remar():
@@ -39,7 +39,7 @@ def a_remar():
         while True:
                 for people in range(4):
                         print("Baja:", listab.pop())
-                        time.sleep(1)
+                        #time.sleep(1)
                 if len(listab) == 0:
                         print("Vacio")
                         print("-")
@@ -50,16 +50,16 @@ def hincha_river():
         listah.append(hincha)
         cond.acquire()
         a_bordo()
-        if len(listab) == 4:
-                cond.wait()
+        #if len(listab) == 4:
+        #        cond.wait()
 
 def hincha_boca():
         hincha = "B"
         listah.append(hincha)
         cond.acquire()
         a_bordo()
-        if len(listab) == 4:
-                cond.wait()
+        #if len(listab) == 4:
+        #        cond.wait()
 
 def barra_brava_river(numeroh):
         """Generacion de hinchas de River"""
@@ -74,13 +74,11 @@ def barra_brava_boca(numeroh):
                 boca.start()
 
 if __name__ == "__main__":
-        barra_river = threading.Thread(target=barra_brava_river, args=(5,))
-        barra_boca = threading.Thread(target=barra_brava_boca, args=(5,))
+        hinchariver = threading.Thread(target=barra_brava_river, args=(2,))
+        hinchaboca = threading.Thread(target=barra_brava_boca, args=(2,))
 
-        barra_river.start()
-        barra_boca.start()
+        hinchariver.start()
+        hinchaboca.start()
 
-        barra_river.join()
-        barra_boca.join()
-
-        print("Llegaron Todos")
+        hinchariver.join()
+        hinchaboca.join()
