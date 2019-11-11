f11ad00649174346ba38dbd1a77ae1be3fdfcd0b
Gianluca <gpersia1998@gmail.com>
2019-11-11 10:52:09 -0300

    update

--- alumnos/56009-Gianluca-Persia/sharingdata.py
+++ alumnos/56009-Gianluca-Persia/sharingdata.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python
-
 import multiprocessing
-
+import os
 resultado = []
+mq = multiprocessing.Queue()
 
 #Defino la funcion en la cual declaro una lista y un bucle para recorrerla e ir multiplicando
-def cuadrado(lista):
-    global resultado
-
-    for num in lista:
-        resultado.append(num * num)
-    print("Resultado: {}".format(resultado))
+def cuadrado(elemento,subindice):
+    print "subindice", subindice, "valor" ,elemento * elemento
+    mq.put([subindice,elemento*elemento])
+    #print os.getpid()
+    return
 
 #Completo el arreglo de mi lista
-lista = [1,2,3,4]
+lista = [1,2,3,4,8,12,4,5]
+p = []
+nro_hijos = len (lista)
+for i in range(nro_hijos):
+    p.append( multiprocessing.Process(target=cuadrado, args=(lista[i],i)))
+    p[i].start()
 
-p1 = multiprocessing.Process(target=cuadrado, args=(lista,))
-p1.start()
-p1.join()
+for i in range(nro_hijos):
+    p[i].join()
+
+print "terminaron todos"
+lista2 = []
+for i in range(nro_hijos):
+    lista2.append ( mq.get())
+print lista, lista2
