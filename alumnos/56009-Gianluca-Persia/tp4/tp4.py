c2e3f2f15b46eb4ff3e308a839d36273fa2c4c26
Gianluca <gpersia1998@gmail.com>
2019-11-12 09:09:26 -0300

    tp4 casi

--- alumnos/56009-Gianluca-Persia/tp4/tp4.py
+++ alumnos/56009-Gianluca-Persia/tp4/tp4.py
@@ -8,7 +8,6 @@ import requests
 import urllib.request, urllib.parse, urllib.error #con las librerias urllib me facilita el trabajo con URLS y contienen funciones que permiten la descarga de archivos
 from PIL import Image #con la libreria PIL puedo manipular imagenes 
 from bs4 import BeautifulSoup #con esta libreria puedo hacer pulling de datos de html o de archivos xml
-#import urllib.error
 from urllib.request import urlopen #la función recibe una URL, ya sea como cadena de texto o como un objeto Request y su objeto de retorno depende del tipo de URL que se le pase como argumento
 import mkred, mkgreen, mkblue
 
@@ -47,7 +46,7 @@ for i in range(len(imageSources)):
     newUrl = parsedUrl.scheme + "://" + parsedUrl.netloc + parsedUrl.path #devuelve los elementos de la estructura de la url
     imageSources[i] = newUrl
 
-path = 'images/'
+path = 'imagenes descargadas/'
 cantimages = []
 
 for i in range(len(imageSources)):
@@ -58,8 +57,8 @@ for url in imageSources:
     if (url[cantidad-3::] == "jpg"):
         try:
             nameimg= cantimages.pop()
-            urllib.request.urlretrieve(url, ruta+str(nameimg)+".jpg")
-            pathimg = 'images/'+str(nameimg)
+            urllib.request.urlretrieve(url, path+str(nameimg)+".jpg")
+            pathimg = 'imagenes descargadas/'+str(nameimg)
             img = Image.open(pathimg+".jpg").convert('RGB').save(pathimg+".ppm") #me ayuda a convertir la imagen
             os.remove(pathimg+".jpg")
             mkred.red(pathimg)
@@ -71,8 +70,8 @@ for url in imageSources:
     if (url[cantidad-3::] == "png"):
         try:
             nameimg= cantimages.pop()
-            urllib.request.urlretrieve(url, ruta+str(nameimg)+'.png')
-            pathimg = 'images/'+str(nameimg)
+            urllib.request.urlretrieve(url, path+str(nameimg)+'.png')
+            pathimg = 'imagenes descargadas/'+str(nameimg)
             img = Image.open(pathimg+".png").convert('RGB').save(pathimg+".ppm") #me ayuda a convertir la imagen
             os.remove(pathimg+".png")
             mkred.red(pathimg)
